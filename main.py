f7979a17204cb57346dc1ed31e538dfda67a3f36
researcher1741 <166040084+researcher1741@users.noreply.github.com>
2024-10-21 21:04:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,14 @@
 from src.train import *
 
 if __name__ == '__main__':
+    import numpy as np
     cases = [
+            {'activation': 'leaky', 'encoding': 'carca', 'nmax': 0.0001, 'ntype': 2.0},
+            {'activation': 'leaky', 'encoding': 'conRotatory', 'nmax': 0.0001, 'ntype': 2.0},
+            {'activation': 'leaky', 'encoding': 'RMHA4', 'nmax': 0.0001, 'ntype': 2.0},
+            {'activation': 'leaky', 'encoding': 'carca', 'nmax': 0.1, 'ntype': 2.0},
+            {'activation': 'leaky', 'encoding': 'conRotatory', 'nmax': 0.1, 'ntype': 2.0},
+            {'activation': 'leaky', 'encoding': 'RMHA4', 'nmax': 0.1, 'ntype': 2.0},
     ]
     for case in cases:
-        hidden_act, encoding, max_norm, norm_type, dataset, need = case
-        print(callme(hidden_act, encoding, max_norm, norm_type, dataset, need))
-
-        # ("silu", "LongRMHA4", 0.0001, 2.0, "fashion", 6),
-        # ("leaky", "LongRMHA4", 0.0001, 2.0, "men", 6),
-        # ("silu", "LongRMHA4", 0.0001, 2.0, "men", 6),
-        # ("leaky", "LongRMHA4", 0.0001, 2.0, "fashion", 6),
-        # ("leaky", "ROPEMHA", None, 2.0, "games", 6),
-        # ("silu", "ROPEMHA", None, 2.0, "games", 6),
-        # ("leaky", "RMHA4", None, 2.0, "games", 6),
-        # ("silu", "RMHA4", None, 2.0, "games", 6),
-        # ("leaky", "conlearnt", None, 2.0, "games", 6),
-        # ("silu", "conlearnt", None, 2.0, "games", 6),
-        # ("leaky", "learnt", None, 2.0, "games", 6),
-        # ("silu", "learnt", None, 2.0, "games", 6),
-        # ("leaky", "carca", None, 2.0, "games", 6),
-        # ("silu", "carca", None, 2.0, "games", 6),
-        # ("leaky", "con", None, 2.0, "games", 6),
-        # ("silu", "con", None, 2.0, "games", 6),
-        # ("leaky", "nocon", None, 2.0, "games", 6),
-        # ("silu", "nocon", None, 2.0, "games", 6),
-        # ("leaky", "conROPE", None, 2.0, "games", 6),
-        # ("silu", "conROPE", None, 2.0, "games", 6),
-        # ("leaky", "ROPE", None, 2.0, "games", 6),
-        # ("silu", "ROPE", None, 2.0, "games", 6),
-        # ("leaky", "ROPEMHAONE", None, 2.0, "games", 6),
-        # ("silu", "ROPEMHAONE", None, 2.0, "games", 6),
-        # ("leaky", "ROPEMHAONE", 0.0001, 2.0, "fashion", 6),
-        # ("silu", "ROPEMHAONE", 0.0001, 2.0, "fashion", 6),
-        # ("leaky", "ROPEMHAONE", 0.0001, 2.0, "men", 6),
-        # ("silu", "ROPEMHAONE", 0.0001, 2.0, "men", 6),
-        # ("leaky", "ROPEMHAONE", 0.0001, 2.0, "beauty", 3),
-        # ("silu", "ROPEMHAONE", 0.0001, 2.0, "beauty", 3),
-        # ("leaky", "LongconROPE", None, 2.0, "games", 6),
-        # ("silu", "LongconROPE", None, 2.0, "games", 6),
-        # ("leaky", "LongROPE", None, 2.0, "games", 6),
-        # ("silu", "LongROPE", None, 2.0, "games", 6),
+        print(callme(case['activation'], case['encoding'], case['nmax'], case['ntype'], 'submen3', 0, 6))
